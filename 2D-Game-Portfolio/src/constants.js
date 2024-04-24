e8c6fcb84e02121dd6252d06faf7cae4ca49bb85
Kristófer Birgir Hjörleifsson <kbh15@hi.is>
2024-04-24 16:29:28 +0000

    fyrsta commit

---
+++ 2D-Game-Portfolio/src/constants.js
@@ -0,0 +1,14 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    Here is my <a href="https://github.com/kristoferbirgir" target="_blank">Github</a>!`,
+  "cs-degree": `These are my socials, <a href="https://www.instagram.com/kristoferbirgir" target="_blank">Instagram</a>!, 
+  <a href="https://www.linkedin.com/in/kristoferbirgir/" target="_blank">LinkedIn</a>! <a href="https://twitter.com/Kristoferbirgir" target="_blank">Twitter</a>!  `, 
+  "sofa-table": ``,
+  tv: ``,
+  resume: ``,
+  projects: ``,
+  library: ``,
+  exit: ``,
+};
